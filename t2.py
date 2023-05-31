d4f212cc044108700e18ec48adfdb00e216c4158
imadul <imadulhasanctis@gmail.com>
2023-05-31 04:52:04 -0400

    pattern_loops

---
+++ t2.py
@@ -0,0 +1,17 @@
+from turtle import*
+
+speed ('slowest')
+
+for i in range(5):
+    pencolor('red')
+    pensize(5)
+    fd(200)
+    lt(360/5)
+    for j in range(6):
+        pencolor('blue')
+        pensize(1)
+        fd(60)
+        lt(360/6)
+        write(f'{i},{j}')
+
+mainloop()        
---
+++ t4.py
@@ -0,0 +1,8 @@
+from turtle import*
+speed('fast')
+pencolor('red')
+
+for i in range(60):
+    fd(i*5+3)
+    lt(360/6)
+mianloop()
